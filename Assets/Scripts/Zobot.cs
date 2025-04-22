8530b3fe745d4c62e5aeee1d17b47574d3d667be
zozozofia <waiyantoemyint1@gmail.com>
2025-04-22 23:31:26 +0630

    Add Zobot.cs base script

---
+++ Assets/Scripts/Zobot.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Zobot : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Zobot initialized!");
+    }
+
+    void Update()
+    {
+        // AI logic များအတွက် ကွက်ကျယ်နေရာ
+    }
+}
+
